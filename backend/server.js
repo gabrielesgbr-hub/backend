3665dccf3c7db831afc41f6b75e3b9638272d3fc
Gabriel Gonzalez <gabrielgonzalez@gmail.com>
2025-11-12 20:00:35 -0600

    render commit

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
 const {errorHandler} = require('./middleware/errorMiddleware')
+const cors = require('cors')
 
 const port = process.env.PORT || 5000 //el puerto de la app la toma de una variable de entorno (PORT del archivo .env) y si no la encuentra utiliza el puerto 5000
 
 connectDB() //Conexion a la base de datos
 
 const app = express() //creo la app la cual es una constancia nueva de express
+
+app.use(cors()) //cors para conectarnos a un sitio web
 
 app.use(express.json()) //envia y recibe datos en forma de JavaScript Object Notation
 app.use(express.urlencoded({extended:false}))
